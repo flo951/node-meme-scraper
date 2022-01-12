37a2dc674aa43d4b9de05cc8c9a94f1ab6471aa3
Florian Görlich <florian.goerlich95@gmail.com>
2022-01-12 17:28:24 +0100

    Implement axios and cheerio to get access to website data

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-import { axios } from 'axios';
+import cheerio from 'cheerio';
+import axios from 'axios';
+import fs from 'fs';
 
-import { URLSearchParams } from 'url';
-const params = new URLSearchParams({ img: 'src' });
-axios.post(
-  'https://memegen-link-examples-upleveled.netlify.app/',
-  params.toString(),
-);
+// URL to be scraped for images
+const url = 'https://memegen-link-examples-upleveled.netlify.app/';
+
+axios
+  .get(url)
+  .then((response) => {
+    let $ = cheerio.load(response.data);
+
+    $('section').each((index, element) => {
+      console.log($(element).find('img').attr('src'));
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
