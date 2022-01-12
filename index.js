2f8d3f1907b10876c64afc2b4fb3a2ca835c0f81
Florian Görlich <florian.goerlich95@gmail.com>
2022-01-12 20:22:52 +0100

    Add a dependency

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import cheerio from 'cheerio';
 import axios from 'axios';
+import fetch from 'node-fetch';
 import fs from 'fs';
 
 // URL to be scraped for images
 const url = 'https://memegen-link-examples-upleveled.netlify.app/';
 
+// get website data from server
 axios
   .get(url)
   .then((response) => {
+    // user cheerio to search in data and store it
     let $ = cheerio.load(response.data);
 
     $('section').each((index, element) => {
+      // logs out one link to first image
       console.log($(element).find('img').attr('src'));
     });
   })
