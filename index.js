68592059c33039af505d5e36c0a640d017f5a01e
Florian Görlich <florian.goerlich95@gmail.com>
2022-01-13 14:18:44 +0100

    Add feature where a loop runs over an array and stores the 10 first src urls

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 import cheerio from 'cheerio';
 import axios from 'axios';
-import fetch from 'node-fetch';
+
 import fs from 'fs';
 
 // URL to be scraped for images
 const url = 'https://memegen-link-examples-upleveled.netlify.app/';
 
-// get website data from server
-axios
-  .get(url)
-  .then((response) => {
-    // user cheerio to search in data and store it
-    let $ = cheerio.load(response.data);
+// get website data from server with axios
+const getWebsite = (urlToScrape) => {
+  axios
+    .get(urlToScrape)
+    .then((response) => {
+      // List of 10 first img to download
+      let imgListDown = [];
+      // use cheerio to search in data and store it
+      let $ = cheerio.load(response.data);
 
-    $('section').each((index, element) => {
-      // logs out one link to first image
-      console.log($(element).find('img').attr('src'));
+      let imgToSave = [];
+      // store each img in an array
+      $('img').each((index, element) => {
+        let imgSource = $(element).attr('src');
+        imgToSave.push(imgSource);
+        //console.log(imgSource);
+      });
+      // loop over the array and push 10 first to another array
+      for (let i = 0; i < 10; i++) {
+        imgListDown.push(imgToSave[i]);
+      }
+      console.log(imgListDown);
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+};
+// for (let i = 0; i < 10; i++) {
+//  console.log(imgList[i]);
+// }
+getWebsite(url);
