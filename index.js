475040f324156f5456220a745fa6371e3139614f
Florian Görlich <florian.goerlich95@gmail.com>
2022-01-14 14:49:58 +0100

    Create Test file with different approach

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import cheerio from 'cheerio';
 import axios from 'axios';
-
+import Path from 'node:path';
 import fs from 'fs';
 
 // URL to be scraped for images
 const url = 'https://memegen-link-examples-upleveled.netlify.app/';
+const folder = '/Users/flo/projects/node-meme-scraper/memes/';
 
 // get website data from server with axios
 const getWebsite = (urlToScrape) => {
@@ -26,6 +27,10 @@ const getWebsite = (urlToScrape) => {
       // loop over the array and push 10 first to another array
       for (let i = 0; i < 10; i++) {
         imgListDown.push(imgToSave[i]);
+        // Download img in memes folder
+        function downloadImage(url, filepath) {
+          return new Promise((resolve, reject) => {});
+        }
       }
       console.log(imgListDown);
     })
@@ -33,7 +38,27 @@ const getWebsite = (urlToScrape) => {
       console.log(error);
     });
 };
-// for (let i = 0; i < 10; i++) {
-//  console.log(imgList[i]);
-// }
+
 getWebsite(url);
+/*
+
+https.get(
+  'https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg?width=300',
+  (res) => {
+    if (res.statusCode === 200) {
+      res
+        .pipe(Fs.createWriteStream(filepath))
+        .on('error', reject)
+        .once('close', () => resolve(filepath));
+    } else {
+      // Consume response data to free up memory
+      res.resume();
+      reject(
+        new Error(
+          `Request Failed With a Status Code: ${res.statusCode}`,
+        ),
+      );
+    }
+  },
+);
+*/
